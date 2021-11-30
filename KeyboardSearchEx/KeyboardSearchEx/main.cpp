f6a862905fbcf2f17f66b3ada15cf9ec98a72905
Marcelo Braga e Silva <71671310+MarceloBragaeSilva@users.noreply.github.com>
2021-11-30 15:53:31 +0100

    Basic search motor working - no freq implemented

--- KeyboardSearchEx/KeyboardSearchEx/main.cpp
+++ KeyboardSearchEx/KeyboardSearchEx/main.cpp
@@ -22,7 +22,7 @@ void print(iteratorT first, iteratorT end) {
 void csv_to_dictionary(Trie* dict) {
     ifstream infile;
     string new_word, frequency;
-    infile.open("wordsandfrequency1000words.csv", ifstream::in);
+    infile.open("wordsandfrequency.csv", ifstream::in);
     if (infile.is_open()) {
         while (infile.good()) {
             getline(infile, new_word, ',');
@@ -35,13 +35,12 @@ void csv_to_dictionary(Trie* dict) {
     }
     else cout << "Error opening file";
     std::cout << "\nDictionary implemented!\n";
+
     std::cout << "Search Test:\n";
     std::cout << "a = " << dict->search("a") << "\n";
     std::cout << "this = " << dict->search("this") << "\n";
-    std::cout << "that = " << dict->search("that") << "\n";
     std::cout << "beaut = " << dict->search("beaut") << "\n";
     std::cout << "asdffg = " << dict->search("asdffg") << "\n";
-    std::cout << "chunegols = " << dict->search("chunegols") << "\n";
     std::cout << "abccc = " << dict->search("abccc") << "\n";
 }
 
@@ -54,60 +53,78 @@ int main() {
 
     vector<string> suggested_words;
     vector<string> current_words;
+    vector<string> added_words;
     char typed_num;
     int characters_typed = 0;
 
+    std::cout << "\n--Test Suggested words--\n";
+    std::cout << "e - exit\nc - clear search\n";
 
     do {
-        std::cout << "\n\nType a number from 2-9: ";
+        std::cout << "\nType number (2-9):";
         std::cin >> typed_num;
         characters_typed++;
         if (typed_num == 'e') break;
-        string typed_chars = keys.find(typed_num)->second;
-        auto end_loop = current_words.size();
+        else if (typed_num == 'c') {
+            current_words.clear();
+            added_words.clear();
+            suggested_words.clear();
+            characters_typed = 0;
+        }
+        else {
+            string typed_chars = keys.find(typed_num)->second;
+            auto end_loop = current_words.size();
 
-        for (int it = 0; it < typed_chars.length(); ++it) { //for each letter that matches number typed
-            if (characters_typed==1) {                                                       //only for first letter added
-                current_words.push_back(string(1,typed_chars[it]));
-                std::cout << string(1, typed_chars[it]) << " added\n";
-            }
-            else {
-                for (int it2 = 0; it2 < end_loop-1; ++it2) {           //when there's words already
-                    //PAREI AQUI
-                    string test = current_words[it2];
-                    test.push_back(typed_chars[it]);
-                    //std::cout << "test = " << test<< " *it = " << *it << "\n";
-                    std::cout << "*it2 + *it = " << test << "\n";
+            for (int it = 0; it < typed_chars.length(); ++it) { //for each letter that matches number typed
+                if (characters_typed == 1) {                                                       //only for first letter added
+                    added_words.push_back(string(1, typed_chars[it]));
+                    //std::cout << string(1, typed_chars[it]) << " added\n";
+                }
+                else {
+                    for (int it2 = 0; it2 < end_loop; ++it2) {           //when there's words already
+                        //std::cout << end_loop << "\n";
+                        //PAREI AQUI
+                        string test = current_words[it2];
+                        test.push_back(typed_chars[it]);
+                        //std::cout << "test = " << test<< " *it = " << *it << "\n";
+                        //std::cout << "*it2 + *it = " << test << "\n";
 
-                    current_words.push_back(test);
+                        added_words.push_back(test);
+                    }
                 }
             }
-        }
-        
-        for (auto it2 = current_words.begin(); it2 != current_words.end(); ++it2) {
-            int search_result = dictionary.search(*it2);
-            if (search_result == -1) {
-                //std::cout << *it2 << " Not a tree branch. Excluded from current_words\n";
-                //current_words.erase(it2);
-            }
-            else if (search_result == 0) {
-                //std::cout << *it2 << " Only a prefix\n";
-            }
-            else if (search_result == 1) {
-                //std::cout << *it2 << " Its a suggested word\n";
-                suggested_words.push_back(*it2);
-            }
-            //std::cout << *it << " ";
-            //for (auto it2 = current_nodes.begin(); it2 != current_nodes.end(); ++it2) {      //for each one of the viable nodes
-                //std::cout<<"Viable Nodes"
-                // it2->search(*it);                                                            //search in the node if it has a children node with letter wanted
 
-        }
-        std::cout << "\nCurrent Words : ";
-        print(current_words.begin(), current_words.end());
-        std::cout << "Suggested Words : ";
-        print(suggested_words.begin(), suggested_words.end());
+            for (auto it2 = added_words.begin(); it2 != added_words.end(); ++it2) {
+                int search_result = dictionary.search(*it2);
+                if (search_result == -1) {
+                    //std::cout << *it2 << " Not a tree branch\n";
+                    //current_words.erase(it2);
+                }
+                else if (search_result == 0) {
+                    //std::cout << *it2 << " Only a prefix\n";
+                    current_words.push_back(*it2);
+                }
+                else if (search_result == 1) {
+                    //std::cout << *it2 << " Its a suggested word\n";
+                    current_words.push_back(*it2);
+                    suggested_words.push_back(*it2);
+                }
+                //std::cout << *it << " ";
+                //for (auto it2 = current_nodes.begin(); it2 != current_nodes.end(); ++it2) {      //for each one of the viable nodes
+                    //std::cout<<"Viable Nodes"
+                    // it2->search(*it);                                                            //search in the node if it has a children node with letter wanted
 
+            }
+
+            current_words = added_words;
+            //std::cout << "\nCurrent Words : ";
+            //print(current_words.begin(), current_words.end());
+            std::cout << "Suggested Words : ";
+            print(suggested_words.begin(), suggested_words.end());
+
+            added_words.clear();
+            suggested_words.clear();
+        }
     } while (true);
 
     return 0;
